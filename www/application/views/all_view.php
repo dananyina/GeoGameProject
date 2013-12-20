16a44dde0843ff5b0c50f19e09c9d869ddeaf006
dananyina <dananyina@mail.ru>
2013-12-20 22:00:02 +0200

    всплівающее описание недоделанное

--- www/application/views/all_view.php
+++ www/application/views/all_view.php
@@ -6,7 +6,7 @@ foreach ($data as $row){
 	echo " \t &nbsp";
 	echo $row["name"];
 	echo "<br>";
-	echo "<div class = 'dascription'>".$row['description']."</div>";
+	echo "<div id=descr_".$row["id"]." class = 'dascription'>".$row['description']."</div>";
 	echo "<br>";
 	echo " \t &nbsp";
 	echo "<img src=".$row["path_img"].">";
